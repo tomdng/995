f73a624c78218479ffc4b5b1a60fb6e29b88fefd
Tom Dong <contact@tomdong.io>
2021-08-10 10:42:51 -0700

    Added more styles to ShopItem component

--- src/data/products.js
+++ src/data/products.js
@@ -1,12 +1,39 @@
 // TODO: Add some types
-export const productList = [
+export default productList = [
 	{
-		description: "",
-    imgUrl: "",
-    itemId: "",
+		description: "An upgraded version of our basic tee with actually good materials",
+    imgUrl: "/assets/hero-alt.jpeg",
+    itemId: "premium-t-shirt",
     itemUrl: "",
-    name: "",
-    price: 0,
+    name: "Premium t-shirt",
+    price: 59.5,
     salePrice: 0,
-	}
+	},
+	{
+		description: "An upgraded version of our basic tee with actually good materials",
+    imgUrl: "/assets/hero-alt.jpeg",
+    itemId: "premium-t-shirt",
+    itemUrl: "",
+    name: "Premium t-shirt",
+    price: 59.5,
+    salePrice: 5.95,
+	},
+	{
+		description: "An upgraded version of our basic tee with actually good materials",
+    imgUrl: "/assets/hero-alt.jpeg",
+    itemId: "premium-t-shirt",
+    itemUrl: "",
+    name: "Premium t-shirt",
+    price: 59.5,
+    salePrice: 0,
+	},
+	{
+		description: "An upgraded version of our basic tee with actually good materials",
+    imgUrl: "/assets/hero-alt.jpeg",
+    itemId: "premium-t-shirt",
+    itemUrl: "",
+    name: "Premium t-shirt",
+    price: 59.5,
+    salePrice: 0,
+	},
 ]
